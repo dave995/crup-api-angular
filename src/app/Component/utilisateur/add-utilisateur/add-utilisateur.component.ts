9883d76730cdc335aea349febee978a8ed56589c
Daouda Thiam <daoudathiam95@gmail.com>
2023-01-26 19:52:14 +0000

    four commit

---
+++ src/app/Component/utilisateur/add-utilisateur/add-utilisateur.component.ts
@@ -0,0 +1,56 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Etudiant } from 'src/app/Component/model/etudiant'
+import { ServiceService } from 'src/app/Component/shared/service.service';
+
+@Component({
+  selector: 'app-add-utilisateur',
+  templateUrl: './add-utilisateur.component.html',
+  styleUrls: ['./add-utilisateur.component.scss']
+})
+export class AddUtilisateurComponent {
+ 
+ objetEtudiant : Etudiant = {
+      
+      id : "",
+      name :"",
+      username : "",
+      email  :"",
+      phone :"",
+      date : "",
+      number: "",
+     
+  };
+
+      id=""
+    photo= ""
+    name=""
+    email=""
+    phone=""
+    date =""
+    number=""
+    username=""
+   
+
+    constructor( private servicesEtudient : ServiceService, private breakpointObserver: BreakpointObserver, private http : HttpClient) {}
+    ngOnit(): void {}
+
+    savedata(){
+      if (this.name =='' || this.email == '' || this.phone == '' || this.username == '' || this.date == '' ) {
+        alert('veuiller saisir tout les champs')
+        return;
+      }
+      this.objetEtudiant.id = this.id
+      this.objetEtudiant.name = this.name
+      this.objetEtudiant.email = this.email
+      this.objetEtudiant.username = this.username
+      this.objetEtudiant.number = this.number
+      this.objetEtudiant.date = this.date
+
+      this.servicesEtudient.saveEtudiant(this.objetEtudiant).subscribe((result)=>{
+
+      })
+    }
+  
+}
--- src/app/Component/utilisateur/utilisateur.component.ts
+++ src/app/Component/utilisateur/utilisateur.component.ts
@@ -4,6 +4,7 @@ import { ServiceService } from '../shared/service.service';
 import { Observable } from 'rxjs';
 import {  map ,shareReplay } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-utilisateur',
@@ -19,15 +20,43 @@ export class UtilisateurComponent implements OnInit {
     map(result => result.matches),
     shareReplay()
   );
+ 
 
   liststudent: Etudiant[] = []
+
+  // objetStudent : Etudiant = {
+  //     id:"",
+  //     photo: "",
+  //     name:"",
+  //     email:"",
+  //     phone:"",
+  //     EnrollNumber:"",
+  //     deleteAdmission: ""
+  // };
   
-  constructor( private servicesEtudient : ServiceService, private breakpointObserver: BreakpointObserver) {}
+    // onDelete(){
+    //   this.http.delete('https://jsonplaceholder.typicode.com/users/'+this.onDelete);
+    // }
+  
+  
+  constructor( private servicesEtudient : ServiceService, private breakpointObserver: BreakpointObserver, private http : HttpClient) {}
   ngOnInit(): void {
     this.servicesEtudient.getStudents().subscribe({
       next : etudiant => {
         this.liststudent = etudiant
       }
     })
+
+    // id=""
+    // photo= ""
+    // name=""
+    // email=""
+    // phone=""
+    // EnrollNumber=""
+    // deleteAdmission= ""
+
+    // onDelete(id){
+    //   this.http.delete('https://jsonplaceholder.typicode.com/users/'+id);
+    // }
   }
 }
